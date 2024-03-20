5b7f0819d7c50754c8a492280e198e2511e2183b
Xiyang Chen <settinghead@gmail.com>
2024-03-20 02:27:50 -0400

    robot and floor

--- game/src/client/3d/components/Floor/Floor.tsx
+++ game/src/client/3d/components/Floor/Floor.tsx
@@ -4,13 +4,13 @@ import { radians } from "../../../utils/angles";
 
 const size = 100;
 
-const floorColor = "#2f313c";
+const floorColor = "#677878";
 
 const Floor: React.FC = () => {
   return (
     <>
       <Plane args={[size, size]} rotation={[radians(-90), 0, 0]} receiveShadow>
-        <meshPhongMaterial attach="material" color={floorColor} />
+        <meshStandardMaterial attach="material" color={floorColor} />
       </Plane>
       {/*<gridHelper args={[size, size]}/>*/}
     </>
